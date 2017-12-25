2e3850363493b786d34aaa85d067ba18a359b49a
yuhsinliu1993 <abcdegtxx.testing@gmail.com>
2017-12-25 21:03:22 +0800

    add codes and update README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# FastFourierTransform
+# Fast Fourier Transform
+
+Implement 2-D Fast Fourier Transform by using Bluestein and Chirp Z Transform Techniques
+
+Reference
+----------
+- https://en.wikipedia.org/wiki/Chirp_Z-transform
+- http://jakevdp.github.io/blog/2013/08/28/understanding-the-fft/
---
+++ run.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+echo "\n==== Running Q1.tif ===="
+time python hw7.py --filename Q1.tif
+
+echo "\n==== Running Q2.tif ===="
+time python hw7.py --filename Q2.tif
+
+echo "\n==== Running Q3.tif ===="
+time python hw7.py --filename Q3.tif
+
+echo "\n==== Running Q4.tif ===="
+time python hw7.py --filename Q4.tif
